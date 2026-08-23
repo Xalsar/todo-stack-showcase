72afda16f94bb2b7a96d38f97d3e2c96dc55fe3b
Xavier Carrera Gimbert <xavicarreragimbert@gmail.com>
2026-08-23 17:34:13 +0200

    fix: enable CORS on BE for cross-origin FE requests

--- apps/BE/src/server.ts
+++ apps/BE/src/server.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 
 import { todosRouter } from './routes/todos.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/todos', todosRouter);
 
